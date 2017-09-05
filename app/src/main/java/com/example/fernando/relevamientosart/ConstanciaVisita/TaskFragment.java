8eeee60bc1466c2a9c2add415b5071c6c6dc8f81
fernandorossello <fmrossello@gmail.com>
2017-09-05 20:32:32 -0300

    Agrupo en packages

--- app/src/main/java/com/example/fernando/relevamientosart/ConstanciaVisita/TaskFragment.java
+++ app/src/main/java/com/example/fernando/relevamientosart/ConstanciaVisita/TaskFragment.java
@@ -1,4 +1,4 @@
-package com.example.fernando.relevamientosart;
+package com.example.fernando.relevamientosart.ConstanciaVisita;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.fernando.relevamientosart.R;
 
 public class TaskFragment extends Fragment {
 
