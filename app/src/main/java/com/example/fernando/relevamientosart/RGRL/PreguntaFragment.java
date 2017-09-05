f33e0166a70aa28e2b32eae6ba5958cad4c3e52d
fernandorossello <fmrossello@gmail.com>
2017-09-05 20:45:38 -0300

    Agrego datepicker a pregunta de RGRL

--- app/src/main/java/com/example/fernando/relevamientosart/RGRL/PreguntaFragment.java
+++ app/src/main/java/com/example/fernando/relevamientosart/RGRL/PreguntaFragment.java
@@ -1,15 +1,24 @@
 package com.example.fernando.relevamientosart.RGRL;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.fernando.relevamientosart.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class PreguntaFragment extends Fragment {
+
+    private final Calendar myCalendar = Calendar.getInstance();
 
     public PreguntaFragment() {
         // Required empty public constructor
@@ -27,8 +36,11 @@ public class PreguntaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pregunta, container, false);
+        View view = inflater.inflate(R.layout.fragment_pregunta, container, false);
+
+        cargarListenerFechaRegul(view);
+
+        return view;
     }
 
 
@@ -40,5 +52,38 @@ public class PreguntaFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    private void cargarListenerFechaRegul(View view) {
+        final EditText tvFechaRegul = view.findViewById(R.id.fechaRegul);
+        final DatePickerDialog.OnDateSetListener dateSetRegulListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(tvFechaRegul);
+            }
+        };
+
+        tvFechaRegul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(v.getContext(), dateSetRegulListener , myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel(EditText editText) {
+        editText.setText(formatearFecha(myCalendar.getTime()));
+    }
+
+    private String formatearFecha(Date date){
+        String myFormat = "dd/MM/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat);
+        return sdf.format(date);
     }
 }
